7143bfc806324dc74e2859b97c8723d80e1632d0
ymaimbo <yacoubmaimbo@gmail.com>
2025-11-18 12:58:49 +0300

    changed back the links

--- src/components/TheNavigation.vue
+++ src/components/TheNavigation.vue
@@ -15,20 +15,20 @@
           </button>
         </div>
         <div class="hidden lg:flex lg:gap-x-12">
-          <router-link v-for="item in navigation" :key="item.name" :href="item.href" class="text-3xl font-semibold text-white">{{ item.name }}></router-link>
+          <a v-for="item in navigation" :key="item.name" :href="item.href" class="text-3xl font-semibold text-white">{{ item.name }}</a>
         </div>
         <div class="hidden lg:flex lg:flex-1 lg:justify-end">
-          <router-link href="/" class="text-sm/6 font-semibold text-white"> <span aria-hidden="true">&rarr;</span> ></router-link>
+          <a href="/" class="text-sm/6 font-semibold text-white"> <span aria-hidden="true">&rarr;</span></a>
         </div>
       </nav>
       <Dialog class="lg:hidden" @close="mobileMenuOpen = false" :open="mobileMenuOpen">
         <div class="fixed inset-0 z-50"></div>
         <DialogPanel class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-gray-900 p-6 sm:max-w-sm sm:ring-1 sm:ring-gray-100/10">
           <div class="flex items-center justify-between">
-            <router-link href="/" class="-m-1.5 p-1.5">
+            <a href="/" class="-m-1.5 p-1.5">
               <span class="sr-only">Your Company</span>
               <img class="h-8 w-auto" src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500" alt="" />
-            ></router-link>
+            </a>
             <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-200" @click="mobileMenuOpen = false">
               <span class="sr-only">Close menu</span>
               <XMarkIcon class="size-6" aria-hidden="true" />
@@ -39,7 +39,7 @@
               <div class="space-y-2 py-6">
               </div>
               <div class="py-6">
-                <a href="#" class="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-white hover:bg-white/5">Log in</a>
+                <a href="/" class="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-white hover:bg-white/5">Log in</a>
               </div>
             </div>
           </div>
